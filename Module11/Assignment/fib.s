5f1bafbaf736c1c1b445b412db4a6c7c62aa6447
Tim <tchen124@rpi-buster-armhf>
2022-11-12 07:04:38 +0000

    add Module11 assigment

---
+++ Module11/Assignment/fib.s
@@ -0,0 +1,75 @@
+.text
+.global main
+main:
+    SUB sp, sp, #4
+    STR lr, [sp]
+
+    LDR r0, =prompt
+    BL printf
+    LDR r0, =format
+    LDR r1, =n
+    BL scanf
+
+    LDR r0, =n
+    LDR r0, [r0]
+    BL fib
+    MOV r2, r0
+
+    LDR r0, =output
+    LDR r1, =n
+    LDR r1, [r1]
+    BL printf
+
+    LDR lr, [sp]
+    ADD sp, sp, #4
+    MOV pc, lr
+
+.data
+    prompt: .asciz "\nEnter a value to get nth number of Fibonacci sequence: "
+    output: .asciz "\nThe %dth number of Fibonacci sequence is %d\n"
+    format: .asciz "%d"
+    n: .word 0
+
+# END main
+
+.text
+fib:
+    SUB sp, sp, #16
+    STR lr, [sp]
+    STR r4, [sp, #4]
+    STR r5, [sp, #8]
+    STR r6, [sp, #12]
+
+    MOV r4, r0
+    MOV r5, #0
+
+    CMP r4, #0
+    ADDEQ r5, r5, #1
+    CMP r4, #1
+    ADDEQ r5, r5, #1
+
+    CMP r5, #1
+    BLT else
+        MOV r0, #1
+        B return
+    else:
+        SUB r0, r4, #1
+        BL fib
+        MOV r6, r0
+        SUB r0, r4, #2
+        BL fib
+        ADD r0, r6, r0
+        B return
+    endif:
+
+    return:
+    LDR lr, [sp]
+    LDR r4, [sp, #4]
+    LDR r5, [sp, #8]
+    LDR r6, [sp, #12]
+    ADD sp, sp, #16
+    MOV pc, lr
+
+.data
+
+#END fib
---
+++ Module11/Assignment/mult.s
@@ -0,0 +1,68 @@
+.text
+.global main
+main:
+    SUB sp, sp, #4
+    STR lr, [sp]
+
+    LDR r0, =prompt
+    BL printf
+    LDR r0, =format
+    LDR r1, =m
+    LDR r2, =n
+    BL scanf
+
+    LDR r0, =m
+    LDR r0, [r0]
+    LDR r1, =n
+    LDR r1, [r1]
+    BL mult
+    MOV r1, r0
+
+    LDR r0, =output
+    BL printf
+
+    LDR lr, [sp]
+    ADD sp, sp, #4
+    MOV pc, lr
+
+.data
+    prompt: .asciz "\nEnter a multiplication in the form of m*n: "
+    output: .asciz "\nYour multiplication is %d\n"
+    format: .asciz "%d*%d"
+    m: .word 0
+    n: .word 0
+
+# END main
+
+.text
+mult:
+    SUB sp, sp, #12
+    STR lr, [sp]
+    STR r4, [sp, #4]
+    STR r5, [sp, #8]
+
+    MOV r4, r0
+    MOV r5, r1
+
+    CMP r4, #1
+    BNE else
+        MOV r0, r5
+        B return
+    else:
+        SUB r0, r4, #1
+        MOV r1, r5
+        BL mult
+        ADD r0, r5, r0
+        B return
+    endif:
+
+    return:
+    LDR lr, [sp]
+    LDR r4, [sp, #4]
+    LDR r4, [sp, #8]
+    ADD sp, sp, #12
+    MOV pc, lr
+
+.data
+
+#END mult
